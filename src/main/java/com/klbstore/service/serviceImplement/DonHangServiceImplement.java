483d2a1e860c208d6010a1a785db9015e47e0cd6
luhoctap2409 <134180810+lunt240923@users.noreply.github.com>
2023-08-07 16:26:24 +0700

    Don Hang - Thong Ke
    Load và xoa Dơn hàng, thống kê doanh thu

---
+++ src/main/java/com/klbstore/service/serviceImplement/DonHangServiceImplement.java
@@ -0,0 +1,69 @@
+package com.klbstore.service.serviceImplement;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.klbstore.dao.ChiTietDonHangDAO;
+import com.klbstore.dao.DonHangDAO;
+import com.klbstore.model.ChiTietDonHang;
+import com.klbstore.model.DonHang;
+import com.klbstore.service.DonHangService;
+
+@Service
+public class DonHangServiceImplement implements DonHangService {
+
+    @Autowired
+    DonHangDAO donHangDAO;
+
+    @Autowired
+    ChiTietDonHangDAO chiTietDonHangDAO;
+
+    @Override
+    public List<DonHang> getAll() {
+        return donHangDAO.findAll();
+    }
+
+    @Override
+    public DonHang getById(Integer donhangId) {
+        return donHangDAO.findById(donhangId).get();
+    }
+
+    @Override
+    public DonHang create(DonHang donhang) {
+        return donHangDAO.save(donhang);
+    }
+
+    @Override
+    public DonHang update(DonHang donhang) {
+        return donHangDAO.save(donhang);
+    }
+
+    @Override
+    public void delete(Integer donhangId) {
+        donHangDAO.deleteById(donhangId);
+    }
+
+     @Override
+    public List<ChiTietDonHang> getCTDonhangByIdDonHang(Integer donHangId) {
+        DonHang donHang = donHangDAO.findById(donHangId).orElse(null);
+        if (donHang != null) {
+          // Lấy danh sách các sản phẩm từ trường có quan hệ @OneToMany với @JsonIgnore chú thích
+          return donHang.getDonHangChiTietDonHangs();
+        }
+        return Collections.emptyList();
+      }
+
+    @Override
+    public List<ChiTietDonHang> getAllChiTietDonHang() {
+        return chiTietDonHangDAO.findAll();
+    }
+
+    @Override
+    public void deleteCTDonHang(Integer chiTietDonHangId) {
+        chiTietDonHangDAO.deleteById(chiTietDonHangId);
+    }
+    
+}
---
+++ target/classes/static/assets/admin/layout/js-ctrl/donhang-ctrl.js
@@ -0,0 +1,162 @@
+app.controller("donhang-ctrl", function ($scope, $http, $location) {
+
+    $scope.form = {};
+    $scope.items = [];
+    $scope.ctDonHang = [];
+    $scope.ctDonHangbyId = {};
+
+    $scope.reset = function () {
+        $scope.form = {
+            ngayDangKy: new Date(),
+            quyenDangNhap: false,
+            gioiTinh: true
+        };
+    }
+
+    $scope.initialize = function () {
+        var url = `${host}/donhang`;
+        $http.get(url).then((result) => {
+            $scope.items = result.data;
+            
+            console.log("Success", result);
+        }).catch((err) => {
+            console.log("Error", err)
+        });
+
+        var url = `${host}/ctdonhang`;
+        $http.get(url).then((result) => {
+            $scope.ctDonHang = result.data;
+            $scope.ctDonHang.forEach(item => {
+                item.donHang.ngayDatHang = new Date(item.donHang.ngayDatHang);
+                item.donHang.ngayGiaoHang = new Date(item.donHang.ngayGiaoHang);
+            })
+            console.log("Success", result);
+        }).catch((err) => {
+            console.log("Error", err)
+        });
+    }
+
+    // load toàn bộ sv từ DB
+    $scope.initialize();
+
+    $scope.edit = function (id) {
+        var url = `${host}/donhang/${id}`;
+        $http.get(url).then((resp) => {
+            $scope.form = resp.data;
+            $scope.form.ngayDatHang = new Date($scope.form.ngayDatHang);
+            $scope.form.ngayGiaoHang = new Date($scope.form.ngayGiaoHang);
+            $scope.getctDonHangByDonHangId($scope.form.donhangId);
+            $(".nav-tabs button:eq(0)").tab("show");
+            console.log("Success", resp)
+        }).catch((err) => {
+            console.log("Error", err)
+        });
+
+    }
+
+    //lấy chi tiet don hang theo đơn hang
+    $scope.getctDonHangByDonHangId = function (id) {
+        var url = `${host}/donhang/${id}/ctdonhang`;
+        $http.get(url).then((resp) => {
+            $scope.ctDonHangbyId = resp.data;
+            
+            console.log("Success", resp)
+        }).catch((err) => {
+            console.log("Error", err)
+        });
+    }
+
+    $scope.create = function () {
+        var item = angular.copy($scope.form);
+        var url = `${host}/donhang`;
+        if(!$scope.myform.$valid) {
+            return;
+        } 
+        else {
+            $http.post(url, item).then((resp) => {
+
+                $scope.items.push(item);
+                $scope.reset();
+                $scope.initialize();  //reload lại items
+                
+                console.log("Success", resp)
+                alert("Thêm thành công!")
+            }).catch((err) => {
+                alert("Lỗi thêm người dùng!")
+                console.log("Error", err)
+            });
+        }
+        
+    }
+
+    $scope.update = function () {
+        var item = angular.copy($scope.form);
+        var url = `${host}/donhang/${$scope.form.donhangId}`;
+        if(!$scope.myform.$valid) {
+            return;
+        } else {
+            $http.put(url, item).then((resp) => {
+                var index = $scope.items.findIndex(item => item.donhangId == $scope.form.donhangId);
+                $scope.items[index] = resp.data;
+                $scope.reset();
+                $scope.initialize();  //reload lại items
+                $(".nav-tabs button:eq(1)").tab("show"); //hiển thị bảng
+                console.log("Success", resp)
+                alert("Cập nhật thành công!")
+            }).catch((err) => {
+                console.log("Error", err)
+                alert("Lỗi cập nhật!")
+            });
+        }
+    }
+
+    //Xóa ChiTietDonHang
+    $scope.deleteCTDonHang = function (donhangId) {
+        var url = `${host}/ctdonhang/${donhangId}`;
+        if(confirm('Xác nhận xóa?')) {
+            $http.delete(url).then((resp) => {
+                var index = $scope.items.findIndex(item => item.donhangId == donhangId);
+                $scope.items.splice(index, 1);
+                $scope.initialize();
+                console.log("Success", resp)
+                alert("Xóa thành công!")
+            }).catch((err) => {
+                console.log("Error", err)
+                alert("Lỗi xóa đơn hàng!")
+            });
+        }
+    }
+
+
+    //Phân trang
+    $scope.pager = {
+        page: 0,
+        size: 5,
+        get items() {
+            let start = this.page * this.size;
+            return $scope.items.slice(start, start + this.size);
+        },
+        get count() {
+            return Math.ceil($scope.items.length / this.size);
+        },
+        first() {
+            this.page = 0;
+        },
+        previous() {
+            this.page--;
+            if (this.page < 0) {
+                this.page = this.count - 1;
+            }
+        },
+        next() {
+            this.page++;
+            if (this.page > this.count - 1) {
+                this.page = 0;
+            }
+        },
+        last() {
+            this.page = this.count - 1;
+        }
+    };
+
+})
